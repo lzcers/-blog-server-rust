19e6b9ee0f5817e0b7448a91b5e9975082faab8c
Zack <lzcers@gmail.com>
2022-04-27 00:46:04 +0800

    add https

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use dotenv::dotenv;
 use std::{env, net::SocketAddr};
 mod db;
 mod services;
+use axum_server::tls_rustls::RustlsConfig;
 
 #[tokio::main]
 async fn main() {
@@ -13,7 +14,7 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 443));
     let blog_db = db::BlogDB::new(&db_url).await.expect("connect blog faild!");
     tracing::debug!("listening on {}", addr);
 
@@ -24,7 +25,12 @@ async fn main() {
         .route("/delete_note", post(services::delete_note))
         .layer(Extension(blog_db));
 
-    axum::Server::bind(&addr)
+    let config =
+        RustlsConfig::from_pem_file(env::var("SSL_CERT").unwrap(), env::var("SSL_KEY").unwrap())
+            .await
+            .unwrap();
+
+    axum_server::bind_rustls(addr, config)
         .serve(app.into_make_service())
         .await
         .unwrap();
